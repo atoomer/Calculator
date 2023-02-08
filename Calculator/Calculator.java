07e6906c9146bf134c0b31a9403010b6a03ca8b0
atoomer <atoomer@quinnipiac.com>
2023-02-07 23:03:58 -0500

    Added code for the prompting and  addition of two doubles.

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a whole number: ");
+        double firstNum = scan.nextDouble();
+        System.out.print("Enter a second number: ");
+        double secondNum = scan.nextDouble();
+
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
         
     }
 }
