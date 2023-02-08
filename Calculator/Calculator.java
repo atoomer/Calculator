5eabc3d5abc1830fa1952d50d7cd6c1f63870704
atoomer <atoomer@quinnipiac.com>
2023-02-08 15:27:18 -0500

    Added subtraction and promts for addition and subtraction.

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -10,7 +10,22 @@ public class Calculator {
         System.out.print("Enter a second number: ");
         double secondNum = scan.nextDouble();
 
-        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
+        System.out.println("\nWould you like to perform addition or subtraction?\n(type \"+\" for addition or \"-\" for subtraction)\n");
+        String operation = scan.next();
+
+        if(operation.equals("+"))
+        {
+            System.out.println(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
+        }
+        else if (operation.equals("-"))
+        {
+            System.out.println(firstNum + " - " + secondNum + " = " + (firstNum-secondNum));
+        }
+        else
+        {
+            System.out.println("Invalid character");
+        }
         
+        scan.close();
     }
 }
